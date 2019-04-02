3adaf298a8af629667e24ee9882ed54cbe50539f
Belgarel <marchand.a@hotmail.fr>
2019-04-02 08:43:30 +0200

    dev(): EntityFramework imports

---
+++ Videotheque/Model/Genre.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore.Sqlite;
+
+namespace Videotheque.Model
+{
+    class Genre
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int GenreId
+        {
+            get;
+            set;
+        }
+
+        public string libelle
+        {
+            get;
+            set;
+        }
+    }
+}
