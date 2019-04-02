d27f0013cc7d5d9893e7cc85a8348c2f70b4c01d
Belgarel <marchand.a@hotmail.fr>
2019-04-02 11:44:48 +0200

    dev(bd): Episode table

---
+++ Videotheque/Model/Episode.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Videotheque.Model
+{
+    class Episode
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Required]
+        public int EpisodeId { get; set; }
+        [Required]
+        public int NumSeason { get; set; }
+        [Required]
+        public int NumEpisode { get; set; }
+        [Required]
+        public int Duration { get; set; }
+        [Required]
+        public string Title { get; set; }
+        public string Description { get; set; }
+        [Required]
+        public DateTime BroadcastDate { get; set; }
+
+        public int MediaId { get; set; }
+        [ForeignKey(nameof(MediaId))]
+        public Genre Media { get; set; }
+    }
+}
